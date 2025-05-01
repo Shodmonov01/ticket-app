996c7b6935a3220ad4d193d4aca77edbeecde2f7
Umida18 <akbarovau42@gmail.com>
2025-05-01 17:53:10 +0500

    login organizatior

--- src/pages/ProfilePage/components/Sheet.tsx
+++ src/pages/ProfilePage/components/Sheet.tsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
 import { Button } from '@/components/ui/button'
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
-import { MoreVertical, Share2, User, LogOut } from 'lucide-react'
+import { Sheet, SheetContent } from '@/components/ui/sheet'
+import { Share2, User, LogOut } from 'lucide-react'
 
 export default function SheetProfile({ open, setOpen }: { open: boolean; setOpen: (value: boolean) => void }) {
     return (
         <div className='flex h-screen items-center justify-center bg-zinc-900 p-4'>
             <Sheet open={open} onOpenChange={setOpen}>
-                <SheetContent
-                    side='bottom'
-                    className='bg-zinc-900 text-white border-t border-zinc-800 rounded-t-xl p-0'
-                >
+                <SheetContent side='bottom' className='bg-black text-white border-t border-zinc-800 rounded-t-xl p-0'>
                     <div className='flex flex-col'>
-                        <div className='space-y-4 p-4'>
+                        <div className='space-y-4 p-4 rounded-xl bg-[#1c232b]'>
                             <button className='flex w-full items-center gap-3 rounded-lg p-2 hover:bg-zinc-800'>
                                 <div className='flex h-8 w-8 items-center justify-center rounded-full bg-zinc-800'>
                                     <Share2 className='h-5 w-5 text-blue-400' />
@@ -35,12 +31,13 @@ export default function SheetProfile({ open, setOpen }: { open: boolean; setOpen
                             </button>
                         </div>
 
-                        <button
-                            className='border-t border-zinc-800 p-4 text-center text-blue-400'
+                        <Button
+                            variant='ghost'
+                            className='bg-[#1c232b] p-4 text-center text-blue-400'
                             onClick={() => setOpen(false)}
                         >
                             Отмена
-                        </button>
+                        </Button>
                     </div>
                 </SheetContent>
             </Sheet>
