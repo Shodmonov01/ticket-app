192c9b35762ed31ae2cc12ed14e5a4cd8919d62f
Umida18 <akbarovau42@gmail.com>
2025-05-02 13:20:57 +0500

    organization role page

--- src/pages/ProfilePage/OrganizationRolePage.tsx
+++ src/pages/ProfilePage/OrganizationRolePage.tsx
@@ -2,12 +2,13 @@ import { useNavigate } from 'react-router-dom'
 
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card'
 import { User } from 'lucide-react'
+import { BottomNav } from '@/components/layout/bottom-nav'
 
 const OrganizationRolePage = () => {
     const navigate = useNavigate()
     return (
         <div>
-            <Card className='bg-gray-800 border-gray-700 mb-4 overflow-hidden'>
+            <Card className='bg-gray-800 border-gray-700 mb-4 overflow-hidden my-10'>
                 <CardHeader className='bg-gradient-to-r from-blue-900 to-indigo-900 pb-8'>
                     <CardTitle className='text-xl text-white'>Выберите тип организации</CardTitle>
                     <CardDescription className='text-blue-100'>
@@ -15,13 +16,12 @@ const OrganizationRolePage = () => {
                     </CardDescription>
                 </CardHeader>
                 <CardContent className='space-y-4 pt-6 -mt-4'>
-                    <div onClick={() => navigate('/ip-form')}>
+                    <div className='flex items-center gap-3' onClick={() => navigate('/ip-form')}>
                         <div>
                             <User className='h-6 w-6 text-cyan-400' />
                         </div>
                         <div>
                             <p className='font-bold text-xl text-white'>ИП</p>
-                            <p className='text-blue-100'>Индивидуальный предприниматель</p>
                         </div>
                     </div>
                     <div className='flex items-center gap-3' onClick={() => navigate('/ooo-form')}>
@@ -30,20 +30,20 @@ const OrganizationRolePage = () => {
                         </div>
                         <div>
                             <p className='font-bold text-xl text-white'>ООО</p>
-                            <p className='text-blue-100'>Общество с ограниченной ответственностью</p>
                         </div>
                     </div>
-                    <div onClick={() => navigate('/self-employed-form')}>
+                    <div className='flex items-center gap-3' onClick={() => navigate('/self-employed-form')}>
                         <div>
                             <User className='h-6 w-6 text-cyan-400' />
                         </div>
                         <div>
                             <p className='font-bold text-xl text-white'>Самозанятый</p>
-                            <p className='text-blue-100'>Плательщик налога на профессиональный доход</p>
                         </div>
                     </div>
                 </CardContent>
             </Card>
+
+            <BottomNav />
         </div>
     )
 }
