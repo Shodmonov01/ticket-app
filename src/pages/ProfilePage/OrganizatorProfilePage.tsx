ee13878d8a2c44189457c4fe50ac0d1c9f70473b
Umida18 <akbarovau42@gmail.com>
2025-05-03 09:16:56 +0500

    .

--- src/pages/ProfilePage/OrganizatorProfilePage.tsx
+++ src/pages/ProfilePage/OrganizatorProfilePage.tsx
@@ -1,3 +1,4 @@
+import { BottomNav } from '@/components/layout/bottom-nav'
 import { useState } from 'react'
 
 const OrganizatorProfilePage = () => {
@@ -5,24 +6,27 @@ const OrganizatorProfilePage = () => {
 
     return (
         <div>
-            <div className='grid grid-cols-2 bg-[#1c232b] rounded-lg overflow-hidden mb-2 p-1'>
-                <button
-                    onClick={() => setActiveTab('upcoming')}
-                    className={`py-2 px-4 text-center font-medium rounded-lg  ${
-                        activeTab === 'upcoming' ? 'text-white bg-[#29333d]' : 'text-gray-400'
-                    }`}
-                >
-                    Распространители
-                </button>
-                <button
-                    onClick={() => setActiveTab('past')}
-                    className={`py-2 px-4 text-center font-medium rounded-lg ${
-                        activeTab === 'past' ? 'text-white bg-[#29333d]' : 'text-gray-400'
-                    }`}
-                >
-                    Мероприятия
-                </button>
-            </div>
+            <main className='container w-full px-4 pt-10'>
+                <div className='grid grid-cols-2 bg-[#1c232b] rounded-lg overflow-hidden mb-2 p-1'>
+                    <button
+                        onClick={() => setActiveTab('upcoming')}
+                        className={`py-2 px-4 text-center font-medium rounded-lg  ${
+                            activeTab === 'upcoming' ? 'text-white bg-[#29333d]' : 'text-gray-400'
+                        }`}
+                    >
+                        Распространители
+                    </button>
+                    <button
+                        onClick={() => setActiveTab('past')}
+                        className={`py-2 px-4 text-center font-medium rounded-lg ${
+                            activeTab === 'past' ? 'text-white bg-[#29333d]' : 'text-gray-400'
+                        }`}
+                    >
+                        Мероприятия
+                    </button>
+                </div>
+            </main>
+            <BottomNav />
         </div>
     )
 }
